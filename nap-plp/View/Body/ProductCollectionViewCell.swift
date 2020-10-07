301c3d396d049bad46ba9f88e060b6deaa658154
chipo mapondera <chipo.mapondera@ynap.com>
2020-10-07 17:19:47 +0100

    UILabels updating and rendering in simulator

--- nap-plp/View/Body/ProductCollectionViewCell.swift
+++ nap-plp/View/Body/ProductCollectionViewCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ProductCollectionViewCell: UICollectionViewCell {
-        
+     
     @IBOutlet weak var productImage: UIImageView!
     @IBOutlet weak var productName: UILabel!
     @IBOutlet weak var productPrice: UILabel!
